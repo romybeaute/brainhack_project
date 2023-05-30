03375f1f8b3bfd060053dc0619e64173c9b1e411
romybeaute <romybeaute.univ@gmail.com>
2023-05-30 18:03:52 +0100

    created def load_data()

--- dynamical_connectivity/load_npz_data.py
+++ dynamical_connectivity/load_npz_data.py
@@ -6,40 +6,40 @@ import os
 ########################################################################################################################################
 
 
-HOMEDIR = os.path.expanduser("~/brainhack/brainhack_project")
-print('HOMEDIR: ',HOMEDIR)
+def load_data():
+    """
+    The max file size accepted is 5GB on OSF and the envelope_signal was 7GB. So had to cut down into 2 files:
+    - alpha, theta, low_beta are in one 
+    - high_beta and wideband are in another
 
+    Source-localized, bandpassed, Hilbert-transformed (video watching data). NB : do not contains RS
+    Each key is of shape : (n_subjects=25,n_ROIs=360,n_times=21250)
+    """
 
-"""
-The max file size accepted is 5GB on OSF and the envelope_signal was 7GB. So had to cut down into 2 files:
-- alpha, theta, low_beta are in one 
-- high_beta and wideband are in another
+    HOMEDIR = os.path.expanduser("~/brainhack/brainhack_project")
+    print('HOMEDIR: ',HOMEDIR)
+    npz_osf_filepath = f"{HOMEDIR}/src_data/src_data_osf" #path to NPZ (Numpy Zip) file format
+    envelope_signal_files = [item for item in os.listdir(npz_osf_filepath)] #get the 2 files of envelope_signal
+    print(envelope_signal_files)
 
-Source-localized, bandpassed, Hilbert-transformed (video watching data). NB : do not contains RS
-Each key is of shape : (n_subjects=25,n_ROIs=360,n_times=21250)
-"""
+    video_watching_dataset = {}  # Dictionary to store the data from both .npz files
+    video_watching_files = {} # Dictionary to store the data from both .npz files
 
-npz_osf_filepath = f"{HOMEDIR}/src_data/src_data_osf" #path to NPZ (Numpy Zip) file format
-envelope_signal_files = [item for item in os.listdir(npz_osf_filepath)] #get the 2 files of envelope_signal
-print(envelope_signal_files)
+    for file in envelope_signal_files:
+        npz_filepath = os.path.join(npz_osf_filepath, file)
+        data = np.load(npz_filepath)
 
-video_watching_dataset = {}  # Dictionary to store the data from both .npz files
-video_watching_files = {} # Dictionary to store the data from both .npz files
+        file_data = {}  # Dictionary to store data for each file
+        for key in data.keys():
+            print(key)
+            file_data[key] = {
+                'data': data[key],
+                'shape': data[key].shape
+            }
+            variable_name = f"{key}_VW_data" # Create a variable name using the key
+            video_watching_dataset[variable_name] = file_data[key]['data'] # Store data in the dictionary using the variable name as the key
 
-for file in envelope_signal_files:
-    npz_filepath = os.path.join(npz_osf_filepath, file)
-    data = np.load(npz_filepath)
+        video_watching_files[file] = file_data
 
-    file_data = {}  # Dictionary to store data for each file
-    for key in data.keys():
-        print(key)
-        file_data[key] = {
-            'data': data[key],
-            'shape': data[key].shape
-        }
-        variable_name = f"{key}_VW_data" # Create a variable name using the key
-        video_watching_dataset[variable_name] = file_data[key]['data'] # Store data in the dictionary using the variable name as the key
-        # globals()[variable_name] = video_watching_dataset[file][key]['data'] # use the globals() function to access the global namespace and create a new variable with the generated variable name.
-
-    video_watching_files[file] = file_data
+    return video_watching_dataset,video_watching_files
 
